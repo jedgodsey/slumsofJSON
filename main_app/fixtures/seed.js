c49315f2016a9f4b1135835ac24eb7128e142545
jedgodsey <jed.godsey@gmail.com>
2021-01-18 09:20:32 -0800

    added users

---
+++ main_app/fixtures/seed.js
@@ -0,0 +1,56 @@
+[
+  {
+    "id": 1,
+    "name": "Robert Fitzgerald Diggs",
+    "username": "RZA",
+    "email": "abbotofthewu@wutang.com"
+  },
+  {
+    "id": 2,
+    "name": "Gary Earl Grice",
+    "username": "GZA",
+    "email": "thegenius@wutang.com"
+  },
+  {
+    "id": 3,
+    "name": "Russell Tyrone Jones",
+    "username": "Ol' Dirty Bastard",
+    "email": "likeitraw@wutang.com"
+  },
+  {
+    "id": 4,
+    "name": "Clifford Smith Jr.",
+    "username": "Method Man",
+    "email": "cheese@wutang.com"
+  },
+  {
+    "id": 5,
+    "name": "Corey Woods",
+    "username": "Raekwon",
+    "email": "thechef@wutang.com"
+  },
+  {
+    "id": 6,
+    "name": "Dennis David Coles",
+    "username": "Ghostface Killah",
+    "email": "tonystarks@wutang.com"
+  },
+  {
+    "id": 7,
+    "name": "Jason Richard Hunter",
+    "username": "Inspectah Deck",
+    "email": "fifthbrother@wutang.com"
+  },
+  {
+    "id": 8,
+    "name": "Lamont Jody Hawkins",
+    "username": "U-God",
+    "email": "goldenarms@wutang.com"
+  },
+  {
+    "id": 9,
+    "name": "Elgin Turner",
+    "username": "Masta Killa",
+    "email": "jameli@wutang.com"
+  },
+]
