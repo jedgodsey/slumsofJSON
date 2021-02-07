d15ec381a8e2426c42ac701fc48f9ad9ece6af77
jedgodsey <jed.godsey@gmail.com>
2021-02-06 21:43:06 -0800

    cleaned up some files

--- main_app/rest_urls.py
+++ main_app/rest_urls.py
@@ -10,28 +10,3 @@ router.register('api/posts', PostViewSet, 'posts')
 router.register('api/comments', CommentViewSet, 'comments')
 
 urlpatterns = router.urls
-
-# urlpatterns = [
-#     path('', views.home, name='home'),
-
-#     # path('users/', views.profiles, name='users'),
-#     # path('users/new/', views.new_profile, name='new_profile'),
-#     # path('users/<int:profile_id>/', views.show_profile, name='show_profile'),
-#     # path('users/<int:profile_id>/edit/', views.edit_profile, name='edit_profile'),
-#     # path('users/<int:profile_id>/delete/', views.delete_profile, name='delete_profile'),
-
-#     # path('posts/', views.posts, name='posts'),
-#     # path('posts/new/', views.new_post, name='new_post'),
-#     # path('posts/<int:post_id>/', views.show_post, name='show_post'),
-#     # path('posts/<int:post_id>/edit/', views.edit_post, name='edit_post'),
-#     # path('posts/<int:post_id>/delete/', views.delete_post, name='delete_post'),
-#     # path('posts/<int:post_id>/add_comment/', views.add_comment, name='add_comment'),
-
-#     # path('comments/', views.comments, name='comments'),
-#     # path('comments/new/', views.new_comment, name='new_comment'),
-#     # path('comments/<int:comment_id>/', views.show_comment, name='show_comment'),
-#     # path('comments/<int:comment_id>/edit/', views.edit_comment, name='edit_comment'),
-#     # path('comments/<int:comment_id>/delete/', views.delete_comment, name='delete_comment'),
-
-#     path('seed/', views.seed)
-# ]
