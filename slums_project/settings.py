8d4dc17f41b3b73e82f8c1f724f78e8e8bc3f1e4
jedgodsey <jed.godsey@gmail.com>
2021-02-08 12:33:47 -0800

    syntax change

--- slums_project/settings.py
+++ slums_project/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 import os
 from pathlib import Path
-import django-on-heroku
+import django_on_heroku
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
